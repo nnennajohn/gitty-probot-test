5d6fe19480dfb221793f3b1d00d468d479c1e9d7
Nnenna John <nnennajohn@gmail.com>
2018-07-05 00:37:23 -0700

    Updates

--- src/components/shared/mixins/tests/hideVisually.test.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import hideVisually from '../hideVisually';
-
-describe('hideVisually', () => {
-  it('should return the CSS in JS', () => {
-    expect({ ...hideVisually() }).toMatchSnapshot();
-  });
-});
